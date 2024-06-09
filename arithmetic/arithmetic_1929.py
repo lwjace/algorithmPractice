ed4dd2f965cd787bde1de56f1ad51362c869d917
woojin <lwjace@naver.com>
2024-06-09 21:13:17 +0900

    06.09

--- arithmetic/arithmetic_1929.py
+++ arithmetic/arithmetic_1929.py
@@ -10,4 +10,7 @@ for i in range(2, int(math.sqrt(N)) +1):
         continue
     for j in range(i + i, N + 1, i):
         A[j] = 0
-    
+
+for i in range (M, N+1):
+ if A[i] != 0:
+    print(A[i])
