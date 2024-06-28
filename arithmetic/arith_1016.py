3a325ebd18c58a166aadccae83791064f1a1334b
woojin <lwjace@naver.com>
2024-06-28 23:57:22 +0900

    06.28

--- arithmetic/arith_1016.py
+++ arithmetic/arith_1016.py
@@ -5,3 +5,6 @@ Check = [False] * (Max - Min +1)
 for i in range(2, int(math,sqrt(Max)+1)):
     pow = i * i
     start_index = int(Min / pow)
+    if Min % pow != 0:
+        start_index += 1
+    for j in range(start_index, int(Max / pow) +1):
