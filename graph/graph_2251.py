3454e6a9317b245692e50c055a950ddbbeec65e2
woojin <lwjace@naver.com>
2024-09-17 23:54:29 +0900

    09.17

--- graph/graph_2251.py
+++ graph/graph_2251.py
@@ -2,3 +2,5 @@ from collections import deque
 
 Sender = [0, 0, 1, 1, 2, 2]
 Receiver = [1, 2, 0, 2, 0, 1]
+now = list(map(int, input().split()))
+visited = [[False for j in range(201)] for i in range(201)]
