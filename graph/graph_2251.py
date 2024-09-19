cf5672a669f9bedbeb816ff18d9bc49cd213e1a5
woojin <lwjace@naver.com>
2024-09-19 23:41:09 +0900

    09.19

--- graph/graph_2251.py
+++ graph/graph_2251.py
@@ -4,3 +4,11 @@ Sender = [0, 0, 1, 1, 2, 2]
 Receiver = [1, 2, 0, 2, 0, 1]
 now = list(map(int, input().split()))
 visited = [[False for j in range(201)] for i in range(201)]
+answer = [False] * 201
+
+def BFS():
+    queue = deque()
+    queue.append((0, 0))
+    visited[0][0] = True
+    while queue:
+        now_Node = queue.popleft()
