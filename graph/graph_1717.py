6b850f2c8318ab4dd70a06bbd82a17883ad212a4
woojin <lwjace@naver.com>
2024-10-13 23:52:58 +0900

    10.13

--- graph/graph_1717.py
+++ graph/graph_1717.py
@@ -1,3 +1,14 @@
 import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(100000)
+N, M = map(int, input().split())
+parent = [0] * (N+1)
+
+def find(a):
+    if a == parent[a]:
+        return a
+    else:
+        parent[a] = find(parent[a])
+        return parent[a]
+
+def
