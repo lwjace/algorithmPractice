9dad568b0277ce4c7d9d5807da4f80a812e65596
woojin <lwjace@naver.com>
2024-11-06 23:49:55 +0900

    11.06

--- graph/graph_1717.py
+++ graph/graph_1717.py
@@ -11,4 +11,15 @@ def find(a):
         parent[a] = find(parent[a])
         return parent[a]
 
-def
+def find(a):
+    if a == parent[a]:
+        return a
+    else:
+        parent[a] = find(parent[a])
+        return parent[a]
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a != b:
+        parent[b] = a
