90d23b0c1e838dad300fb4ce4b14508bcc794f0a
woojin <lwjace@naver.com>
2024-12-12 00:10:50 +0900

    12.12

--- graph/graph1976.py
+++ graph/graph1976.py
@@ -1,3 +1,12 @@
 N = int(input())
 M = int(input())
 dosi = [[0 for j in ragne(N+a)] for i in range(N+1)]
+
+def find(a):
+    if a == parent[a]:
+        return a
+    else:
+        parent[a] = find(parent[a])
+        return parent[a]
+
+def union(a, b):
