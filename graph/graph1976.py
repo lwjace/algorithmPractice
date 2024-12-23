94ff033d7bc40d3d23776aa25393aaaf368bc8ee
woojin <lwjace@naver.com>
2024-12-23 23:41:05 +0900

    12.23

--- graph/graph1976.py
+++ graph/graph1976.py
@@ -10,3 +10,13 @@ def find(a):
         return parent[a]
 
 def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a != b:
+        parent[b] = a
+
+for i in range(1, N+1):
+    dosi[i] = list(map(int, input().split()))
+    dosi[i].insert(0, 0)
+
+route = list(map(int, input().split()))
